92f71926ca9b9f6b7ee56341c077ab904bb96482
ravin00 <ravinbandara76@gmail.com>
2025-09-11 22:51:51 +0530

    feat: Add JwtHelper for token generation functionality

---
+++ AuthService/Helpers/JwtHelper.cs
@@ -0,0 +1,39 @@
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AuthService.Models;
+
+namespace AuthService.Helpers
+{
+    public class JwtHelper
+    {
+        private readonly string _secret;
+
+        public JwtHelper(string secret)
+        {
+            _secret = secret;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Username)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
